526938936c526ababf84f656a7bb005dd5a09bfe
hrdy <sonushashankmot@gmail.com>
2021-12-22 14:32:51 +0530

    third commit

---
+++ test/OpenMarket.test.js
@@ -0,0 +1,38 @@
+const {assert} = require('chai')
+
+
+const OpenMarket = artifacts.require('./OpenMarket');
+
+// check for chai
+require('chai')
+.use(require('chai-as-promised'))
+.should()
+
+contract('KryptoBird', (accounts) => {
+    let contract
+    // before tells our tests to run this first before anything else 
+    before( async () => {
+    contract = await OpenMarket.deployed() 
+    })
+
+    // testing container - describe 
+
+    describe('deployment', async() => {
+        // test samples with writing it 
+        it('deploys successfuly', async() => {
+            const address = contract.address;
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+            assert.notEqual(address, 0x0)
+        })
+        it('has a name', async() => {
+            const name = await contract.name()
+            assert.equal(name, 'Kryptonion')
+        })
+        it('has a symbol', async() => {
+            const symbol = await contract.symbol()
+            assert.equal(symbol, 'KN')
+        })
+    })
+})
