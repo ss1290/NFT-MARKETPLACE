24488fd596f824f508b250a9a99c8a041857efbd
deepanshurajput6815 <85389098+deepanshurajput6815@users.noreply.github.com>
2021-12-22 21:42:02 +0530

    pull request

--- src/contracts/OpenMarket.sol
+++ src/contracts/OpenMarket.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract OpenMarket is ERC721 {
-    constructor() ERC721("Kryptonion", "KN"){}
+    constructor() ERC721("kryptoBird", "KN"){}
+
 }
