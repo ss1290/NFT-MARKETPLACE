00283a2a543a03c9e95b61fc84e147ed4dfc4e47
akhilkashyap1 <75844617+akhilkashyap1@users.noreply.github.com>
2021-12-22 23:34:57 +0530

    mint test
    mint test

--- test/OpenMarket.test.js
+++ test/OpenMarket.test.js
@@ -8,7 +8,7 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('KryptoBird', (accounts) => {
+contract('OpenMarket', (accounts) => {
     let contract
     // before tells our tests to run this first before anything else 
     before( async () => {
@@ -37,6 +37,22 @@ contract('KryptoBird', (accounts) => {
         it('supports interface', async() => {
             const check = await contract.supportsInterface('0x80ac58cd') || contract.supportsInterface('0x5b5e139f')
             assert.equal(check,true)
+        })  
+    })
+
+    describe('minting', async ()=> {
+        it('creates a new token', async () => {
+            const result = await contract.mint()
+          
+            //Success
+           
+            const event = result.logs[0].args
+            assert.equal(event._from, '0x0000000000000000000000000000000000000000', 'from the contract')
+            assert.equal(event._to, accounts[0], 'to is msg.sender')
+
+            //Failure
+            await contract.mint().should.be.rejected;
+            
         })
     })
 })
