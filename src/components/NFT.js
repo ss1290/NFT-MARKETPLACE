731fe85fe9fc47899c5c003c821cbc4b4a0ae17f
Sachin976 <83978822+Sachin976@users.noreply.github.com>
2021-12-26 22:54:58 +0530

    Few tweaks

--- src/components/NFT.js
+++ src/components/NFT.js
@@ -1,13 +1,11 @@
 import React from "react";
-import {useSearchParams,useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 
-const NFT = (props)=>{
-    const[searchParams,setSearchParams] = useSearchParams();
-    const name = searchParams.get('name');
+const NFT = ()=>{
     let params = useParams();
     return(
     <div>    
-        {<h1>{params.NFTname.toLocaleUpperCase()}</h1>}
+        {<h1>{params.NFTname}</h1>}
         <p>This NFT belongs to None</p>
     </div>
 )};
--- src/index.js
+++ src/index.js
@@ -2,12 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AppRouter from "./routers/AppRouter";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render( <AppRouter />, document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
