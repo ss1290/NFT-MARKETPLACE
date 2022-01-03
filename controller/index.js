ccc1468d1741da6b8b700e3e34f252f285636138
Sonu Shashank <sonushashankmot@gmail.com>
2022-01-03 20:16:44 +0530

    profile page changes

--- controller/index.js
+++ controller/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Web3 = require('web3');
 const myContract = require('../src/abis/OpenMarket.json')
 const mysql = require('mysql');
+const cors = require('cors')
 
 //Create connection
 const db = mysql.createConnection({ 
@@ -19,7 +20,8 @@ db.connect((err)=>{
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 const web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
 
@@ -46,6 +48,7 @@ app.get('/contract',async(req,res)=>{
 app.post('/createUser',(req,res) =>{
     let user =req.body
     let sql = 'INSERT INTO User SET ?';
+     console.log(req.body);
      let query = db.query(sql, user,(err,result)=>{
         if(err) throw err;
         console.log(result);
@@ -61,6 +64,15 @@ app.post('/mintToken',async(req,res) =>{
         res.send('token minted');
     });
 });
+
+app.get('/getUser/:address', async(req,res)=>{
+    let sql = `SELECT * FROM User HAVING walletAddress = ${req.params.address}`
+     db.query(sql,(err,result)=>{
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+})
 
 app.get('/getToken/:address',async(req,res) =>{
     let sql = `SELECT * FROM Token HAVING currentOwner='${req.params.address}'`
