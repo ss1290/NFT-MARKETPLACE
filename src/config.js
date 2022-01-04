e9517cd572e61a61a7507acd040c5070b6286a8b
Jay-mac-art <jayantk7722@gmail.com>
2022-01-05 00:06:11 +0530

    profile updation added

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 import contract from "./abis/OpenMarket.json"
-export const address = "0xb9aE43e39971130a1938bD22b5c5726dcB20c0ca";
+export const address = "0xbd1C27F7Aba63eeef3317EC9CF5ff60677eF93Ec";
 export const abi = contract.abi;
