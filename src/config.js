8c7a5c16be618d0bb537876d18760d493a607ee8
Sachin976 <sachinshoff@gmail.com>
2022-01-04 11:37:10 +0530

    changes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 import contract from "./abis/OpenMarket.json"
-export const address = "0x61194652B57970F24C984495204376B3E5A52685";
+export const address = "0x5Bf4984FE608cb8243806DcA5170b8B82f74915A";
 export const abi = contract.abi;
